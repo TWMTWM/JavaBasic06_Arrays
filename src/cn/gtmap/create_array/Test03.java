46b8ffc865a8f86c62697298673b8c2bcb2f9c30
TWM <15152806231@163.com>
2020-01-07 19:31:35 +0800

    访问数组

---
+++ src/cn/gtmap/create_array/Test03.java
@@ -0,0 +1,18 @@
+package cn.gtmap.create_array;
+
+/**
+ * 3、访问数组
+ *
+ */
+public class Test03 {
+	public static void main(String[] args) {
+		int[] a;
+		a = new int[5];
+		a[0] = 1; //下标0，代表数组里的第一个数
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+	}
+
+}
