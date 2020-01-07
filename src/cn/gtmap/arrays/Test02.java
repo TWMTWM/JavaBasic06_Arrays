7e1628ea1d8c4bc7d361e37d4e1f7b3d0f64b712
TWM <15152806231@163.com>
2020-01-07 20:30:52 +0800

    转换为字符串

---
+++ src/cn/gtmap/arrays/Test02.java
@@ -0,0 +1,16 @@
+package cn.gtmap.arrays;
+
+import java.util.Arrays;
+/**
+ * ×ª»»Îª×Ö·û´®
+ * @author Administrator
+ *
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
+        String content = Arrays.toString(a);
+        System.out.println(content);
+  
+    }
+}
