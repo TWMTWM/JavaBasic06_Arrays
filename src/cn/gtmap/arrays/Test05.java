df64edbe11d200d3395290f6520630b5ac844acb
TWM <15152806231@163.com>
2020-01-07 20:33:08 +0800

    判断是否相同

---
+++ src/cn/gtmap/arrays/Test05.java
@@ -0,0 +1,17 @@
+package cn.gtmap.arrays;
+
+import java.util.Arrays;
+
+/**
+ * ≈–∂œ «∑Òœ‡Õ¨
+ * @author Administrator
+ *
+ */
+public class Test05 {
+    public static void main(String[] args) {
+        int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
+        int b[] = new int[] { 18, 62, 68, 82, 65, 8 };
+ 
+        System.out.println(Arrays.equals(a, b));
+    }
+}
