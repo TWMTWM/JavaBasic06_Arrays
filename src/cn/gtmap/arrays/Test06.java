1e61218d2181ffd356cc2256060060af0fe1fc81
TWM <15152806231@163.com>
2020-01-07 20:33:39 +0800

    填充

---
+++ src/cn/gtmap/arrays/Test06.java
@@ -0,0 +1,19 @@
+package cn.gtmap.arrays;
+
+import java.util.Arrays;
+
+/**
+ * Моід
+ * @author Administrator
+ *
+ */
+public class Test06 {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+  
+        Arrays.fill(a, 5);
+  
+        System.out.println(Arrays.toString(a));
+  
+    }
+}
