afa0090810991cdc1202b7c05e1ff8d424f6b5de
TWM <15152806231@163.com>
2020-01-07 19:28:50 +0800

    创建数组

---
+++ src/cn/gtmap/create_array/Test02.java
@@ -0,0 +1,13 @@
+package cn.gtmap.create_array;
+
+public class Test02 {
+	public static void main(String[] args) {
+		//声明一个引用
+		int[] a;
+		//创建一个长度是5的数组，并且使用引用a指向该数组
+		a= new int[5];
+		
+		int[] b = new int[5];//声明的同时，指向一个数组
+		int c[] = new int[5];
+	}
+}
